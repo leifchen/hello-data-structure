d9b35b6525309372ee28268f6df924f3599fae6f
LeifChen <leifchen90@gmail.com>
2018-08-31 11:26:45 +0800

    feat: add binary search tree

---
+++ src/test/java/com/chen/tree/BinarySearchTreeTest.java
@@ -0,0 +1,70 @@
+package com.chen.tree;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * BinarySearchTreeTest
+ *
+ * @Author LeifChen
+ * @Date 2018-08-30
+ */
+public class BinarySearchTreeTest {
+
+    BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+
+    @Before
+    public void init() {
+        int[] nums = {5, 3, 1, 2, 4, 9, 7, 6, 8, 10};
+        for (int num : nums) {
+            bst.add(num);
+        }
+    }
+
+    @Test
+    public void isEmpty() {
+        assertEquals(10, bst.size());
+        assertFalse(bst.isEmpty());
+    }
+
+    @Test
+    public void contains() {
+        assertTrue(bst.contains(2));
+        assertFalse(bst.contains(11));
+    }
+
+    @Test
+    public void order() {
+        System.out.println(bst);
+        System.out.println("preOrder: ");
+        bst.preOrder();
+        System.out.println("preOrderNR");
+        bst.preOrderNR();
+        System.out.println("inOrder: ");
+        bst.inOrder();
+        System.out.println("postOrder: ");
+        bst.postOrder();
+        System.out.println("levelOrder: ");
+        bst.levelOrder();
+    }
+
+    @Test
+    public void removeMin() {
+        assertEquals(1, bst.removeMin().intValue());
+        bst.inOrder();
+    }
+
+    @Test
+    public void removeMax() {
+        assertEquals(10, bst.removeMax().intValue());
+        bst.inOrder();
+    }
+
+    @Test
+    public void remove() {
+        bst.remove(3);
+        System.out.println(bst);
+    }
+}
