2fe1664b0f9f312af19068aabcb23108283746e4
LeifChen <leifchen90@gmail.com>
2018-09-05 17:37:05 +0800

    feat: add priority queue

---
+++ src/main/java/com/chen/queue/PriorityQueue.java
@@ -0,0 +1,43 @@
+package com.chen.queue;
+
+import com.chen.heap.MaxHeap;
+
+/**
+ * 优先级队列
+ *
+ * @Author LeifChen
+ * @Date 2018-09-04
+ */
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue() {
+        maxHeap = new MaxHeap<>();
+    }
+
+    @Override
+    public int getSize() {
+        return maxHeap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return maxHeap.isEmpty();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return maxHeap.extractMax();
+    }
+
+    @Override
+    public E getFront() {
+        return maxHeap.findMax();
+    }
+}
